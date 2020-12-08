d62f9c104d428e09b63b8ec5dbce29493c5953cb
lukasklima <lukas.klima@live.at>
2020-12-08 17:53:26 +0100

    Add lexical task data and update data documentation

--- R/stroop.R
+++ R/stroop.R
@@ -1,17 +1,17 @@
-#' Stroop Dataset from Rouder
+#' Stroop Dataset from Von Bastian et al. (2015)
 #'
-#' The dataset from Rouder (20xx) on the Stroop task.
+#' The dataset from on Von Bastian et al. (2015) on the Stroop task.
 #'
 #' @format A \code{data.frame} of 11245 rows and 7 columns
 #' \describe{
-#'  \item{ID}{Numeric values giving the participant ID, 1-121}
+#'  \item{ID}{Numeric values giving the participant ID: 1-121}
 #'  \item{congruency}{Factor giving one of the two conditions: congruent and incongruent}
 #'  \item{RT}{Numeric values of the response times in milliseconds}
 #'  \item{accuracy}{Numeric values giving the accuracy of the response, here only 1 = correct}
-#'  \item{cond}{Numeric values giving the condition, 1 = congruent, 2 = incongruent}
+#'  \item{cond}{Numeric values giving the condition: 1 = congruent, 2 = incongruent}
 #'  \item{trial}{Numeric values indicating the trial number of the participant}
 #'  \item{rt}{Numeric values giving the response time in seconds}
 #'  }
 #'
-#' @source Data from Rouder (20xx), retrieved from https://raw.githubusercontent.com/PerceptionCognitionLab/data0/master/contexteffects/FlankerStroopSimon/LEF_stroop.csv
+#' @source Data from Von Bastian et al. (2015), retrieved from \url{https://raw.githubusercontent.com/PerceptionCognitionLab/data0/master/contexteffects/FlankerStroopSimon/LEF_stroop.csv}
 "stroop"
